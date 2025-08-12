5f99b44bfcf282b2813aaa4f08b4b2bc8875684b
blukai <20866892+blukai@users.noreply.github.com>
2025-08-12 21:49:35 +0200

    ntree: bye bye

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,6 @@ pub use dungers_charsor as charsor;
 #[cfg(feature = "genvec")]
 pub use dungers_genvec as genvec;
 
-#[cfg(feature = "ntree")]
-pub use dungers_ntree as ntree;
-
 #[cfg(feature = "rangealloc")]
 pub use dungers_rangealloc as rangealloc;
 
